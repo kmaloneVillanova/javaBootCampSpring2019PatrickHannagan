028b005ebde0797446edd0aa14290093f82880f9
Patrick Hannagan <patrick.robert.hannagan.33@gmail.com>
2019-04-29 09:36:27 -0400

    Added second attempt at final project

--- AndroidApps/FinalProject/app/src/main/java/com/example/finalproject/IngredientList.java
+++ AndroidApps/FinalProject/app/src/main/java/com/example/finalproject/IngredientList.java
@@ -1,5 +1,6 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,26 +10,45 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class IngredientList extends AppCompatActivity {
 
-    public static final String EXTRA_REPLY = "com.example.finalproject.IngredientList.extra.reply";
+    public static final String EXTRA_REPLY = "com.example.FinalProject.IngredientList.extra.REPLY";
 
+    ArrayList<String> selectedIngredients = new ArrayList<>();
 
-    public boolean selectedIngredients[] = {false, false, false, false, false, false, false, false,
-            false, false, false, false, false};
+/*
+    public boolean[] selectedIngredients = {false, false, false, false, false, false, false, false,
+            false, false, false, false, true};
+*/
+
+    /*public boolean[] selectedIngredients;*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient_list);
+        Intent intent = getIntent();
+        boolean[] message = intent.getBooleanArrayExtra(MainActivity.EXTRA_MESSAGE);
         String[] ingredients = {"Vodka", "Rum", "Whiskey", "Tequila", "Gin", "Lemon Juice",
                 "Lime Juice", "Simple Syrup", "Orange Juice", "Soda Water", "Tonic Water",
                 "Dry Vermouth", "Sweet Vermouth", "Bitters"};
+/*        for(int i=0;i<ingredients.length;i++){
+            selectedIngredients[i]=false;
+        }*/
         ListAdapter listOfIngredientsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, ingredients);
         ListView listOfIngredients = (ListView) findViewById(R.id.listOfIngredients);
         listOfIngredients.setAdapter(listOfIngredientsAdapter);
 
-        listOfIngredients.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+    /*        for(int i=0;i<ingredients.length;i++){
+                if(selectedIngredients[i]==true){
+                    listOfIngredients.setItemChecked(i, true);
+                }
+
+            }*/
+/*        listOfIngredients.setOnItemClickListener(new AdapterView.OnItemClickListener(){
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         String selectedIngredient = String.valueOf(parent.getItemAtPosition(position));
@@ -36,12 +56,13 @@ public class IngredientList extends AppCompatActivity {
 
                     }
                 }
-        );
+        );*/
 
         listOfIngredients.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectedIngredients[position]=true;
+                String selectedIngredient = String.valueOf(parent.getItemAtPosition(position));
+                selectedIngredients.add(selectedIngredient);
             }
 
             @Override
@@ -49,5 +70,14 @@ public class IngredientList extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void returnIngredients(View view){
+/*        boolean[] reply = selectedIngredients;*/
+        Intent replyIntent = new Intent();
+/*        replyIntent.putExtra(EXTRA_REPLY, reply);*/
+        replyIntent.putExtra(EXTRA_REPLY, selectedIngredients);
+        setResult(RESULT_OK,replyIntent);
+        finish();
     }
 }
--- AndroidApps/FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
+++ AndroidApps/FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,8 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.FinalProject.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+
+/*    private boolean[] selectedIngredients = {false, true, false, false, false, false, false, false,
+            false, false, false, false, false};*/
+
+    ArrayList<String> selectedIngredients = new ArrayList<String>();
+    ArrayList<String> ingredientsToDisplay = new ArrayList<String>();
+
+    /*private boolean[] selectedIngredients;*/
+    /*public String[] ingredientsToDisplay;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +33,52 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchIngredientList(View view) {
         Intent intent = new Intent(this, IngredientList.class);
-        startActivity(intent);
+        intent.putExtra(EXTRA_MESSAGE, selectedIngredients);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
 
     public void printSelectedIngredients(View view) {
+        String[] ingredientsToDisplay = {"Vodka", "Rum", "Whiskey", "Tequila", "Gin", "Lemon Juice",
+                "Lime Juice", "Simple Syrup", "Orange Juice", "Soda Water", "Tonic Water",
+                "Dry Vermouth", "Sweet Vermouth", "Bitters"};
 
+        selectedIngredients.add("Vodka");
+        selectedIngredients.add("Lime Juice");
+        selectedIngredients.add("Soda Water");
+
+        /*Toast.makeText(MainActivity.this, ingredients[3], Toast.LENGTH_LONG);*/
+/*        for(int i=0;i<14;i++){
+            if(selectedIngredients[i]){
+                *//*ingredientsToDisplay[i]=ingredients[i];*//*
+                String selectedIngredient = String.valueOf(ingredients[i]);
+                Toast.makeText(MainActivity.this, selectedIngredient, Toast.LENGTH_LONG);
+            } else {
+                *//*ingredientsToDisplay[i]="N/A";*//*
+            }
+        }*/
+/*        ListAdapter listOfIngredientsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, ingredientsToDisplay);
+        ListView listOfIngredients = (ListView) findViewById(R.id.listOfIngredients);
+        listOfIngredients.setAdapter(listOfIngredientsAdapter);*/
+
+        ListAdapter listOfIngredientsAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_expandable_list_item_1,
+                selectedIngredients);
+        ListView listOfIngredients = (ListView) findViewById(R.id.listOfIngredients);
+        listOfIngredients.setAdapter(listOfIngredientsAdapter);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == TEXT_REQUEST){
+            if(resultCode == RESULT_OK){
+                selectedIngredients = data.getStringArrayListExtra(IngredientList.EXTRA_REPLY);
+/*                boolean[] reply = data.getBooleanArrayExtra(IngredientList.EXTRA_REPLY);
+                selectedIngredients=reply;*/
+
+            }
+        }
     }
 }
